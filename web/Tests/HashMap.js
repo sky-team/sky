1f00c7891326aa2c30d82357c6be7b77128af870
Yazan <Yazan@Yazan-PC>
2013-04-22 22:52:19 -0700

    Here More Updates to the drawing engine some more features added also the communication protocol is implemented using Javascript.

---
+++ web/Tests/HashMap.js
@@ -0,0 +1,148 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function Pair(k,v){
+    this.key = k;
+    this.value = v;
+    this.next = null;
+    this.prev = null;
+}
+
+function HashMap(){
+    this.first = null;
+    this.last = null;
+    this.count = 0;   
+    
+    this.comparetor = function(key1,key2){
+        return key1 == key2;
+    }
+}
+
+HashMap.prototype.add = function(k,v){
+    if(this.first == null){
+        this.first = new Pair(k,v);
+        this.first.next = null;
+        this.first.prev = null;
+        this.last = this.first;
+    }else{
+        this.last.next = new Pair(k,v);
+        this.last.next.prev = this.last;
+        this.last.next.next = null;
+        this.last = this.last.next;
+    }
+        
+    this.count ++;
+}
+    
+HashMap.prototype.isEmpty = function (){
+    return this.first == null;
+}
+    
+HashMap.prototype.size = function (){
+    return this.count;
+}
+    
+HashMap.prototype.get =  function(key){
+    var temp = this.first;
+        
+    while(temp){
+        if(this.comparetor(temp.key, key))
+            break;
+        temp = temp.next;
+    }
+        
+    return temp.value;
+}
+
+HashMap.prototype.remove = function (key){
+    var temp = this.first;
+        
+    while(temp){
+        if(this.comparetor(temp.key, key)){
+            this.count --;
+            if(temp.prev){
+                if(temp.next){
+                    temp.prev.next = temp.next;
+                    temp.next.prev = temp.prev;
+                }else{
+                    temp.prev.next = null;
+                    this.last = temp.prev;
+                }
+            }else{
+                if(temp.next){
+                    temp.next.prev = null;
+                    this.first = temp.next;
+                }else{
+                    this.first = null;
+                    this.last = null;
+                }
+            }
+                
+            return temp.value;
+        }
+            
+        temp = temp.next;
+    }
+        
+    return null;
+}
+
+HashMap.prototype.forEach = function (callback){
+    var temp = this.first;
+    
+    while(temp){
+        callback(temp.key,temp.value);
+        temp = temp.next;
+    }
+}
+
+HashMap.prototype.keySet = function (){
+    var set = new ArrayList();
+    
+    var temp = this.first;
+    
+    while(temp){
+        set.add(temp.key);
+        temp = temp.next;
+    }
+}
+
+HashMap.prototype.valueSet = function (){
+    var set = new ArrayList();
+    
+    var temp = this.first;
+    
+    while(temp){
+        set.add(temp.value);
+        temp = temp.next;
+    }
+}
+
+HashMap.prototype.toJson = function(){
+    
+    var temp = this.first;
+    var json = "{";
+    while(temp){
+        var val = "";
+
+        if(temp.value instanceof HashMap){
+            val = temp.value.toJson();
+            json += "\"" + temp.key + "\"" + ":" + val;
+        }
+        else{
+            val = temp.value; 
+            var add = "";
+            if(temp.next != null)
+                add = ",";
+            json += "\"" + temp.key + "\"" + ":" + "\"" + val + "\"" + add;
+        }
+        
+        temp = temp.next;
+    }
+    
+    json += "}";
+    
+    return json;
+}
--- web/Tests/Text.js
+++ web/Tests/Text.js
@@ -1,7 +1,3 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 function Text(text){
 
@@ -11,47 +7,134 @@ function Text(text){
     this.text = text;
     
     this.fontFamily = "Arial";
-    this.fontSize = 20;
+    this.fontSize = 10;
     
     this.effects = new Array();
+    
+    this.element = null;
 }
 
 Text.prototype = new Drawable();
 
-Text.prototype.setup = function (ctx){
-    var best = getRealSize(ctx,this.text,this.fontSize,this.fontFamily);
-    this.width = best.width;
-    this.height = best.height;
-}
-
 Text.prototype.hasPoints = function(mx,my){
-    return (mx >= this.x && mx < (this.x+this.width)) && (my >= this.y && my <= (this.y+this.height));
-}
-
-Text.prototype.draw = function(ctx){
-    
-    for(var i = 0 ; i < this.effects.length ;i++){
-        this.effects[i].startEffects(ctx);
-    }
-
-    ctx.strokeStyle= this.drawColor;
-    ctx.font=this.fontSize+"px "+this.fontFamily;
-    ctx.fillText(this.text,this.x,this.y);
-    ctx.stroke();
-    
-    for(var i = 0 ; i < this.effects.length ;i++){
-        this.effects[i].endEffects(ctx);
-    }
-}
-
-Text.prototype.clear = function(ctx){
-    this.fontSize = getBestSize(this.text, this.fontFamily, this.width, this.height);
-    ctx.strokeStyle= this.clearColor;
-    ctx.font=this.fontSize+"px "+fontFamily;
-    ctx.fillText(text,this.x,this.y);
-    ctx.stroke();
+    return this.element == null ? false : this.element.isPointInside(mx,my);
 }
 
 Text.prototype.toStr = function(){
     return "Text:["+this.text+"] , "+"X:"+this.x+" , Y:"+this.y + " , Width:"+this.width+" , Height:"+this.height;
+}
+
+Text.prototype.update = function(){
+}
+
+Text.prototype.destroyElement = function(){
+    
+    if(this.element == null)
+        return;
+    
+    this.element.remove();
+    
+    this.element = null;
+}
+
+Text.prototype.createElement = function(paper){
+    
+    if(this.element != null)
+        return;
+    this.element = paper.text(this.x ,this.y,this.text);
+    var font = this.fontSize+"px "+this.fontFamily;
+    this.element.attr(
+        {
+            "font" : font,
+            "fill" : this.drawColor,
+            'text-anchor' : 'start'
+        }
+    );
+    var b = this.element.getBBox();
+    
+    this.width = Math.abs(b.x2) - Math.abs(b.x);
+    this.height = Math.abs(b.y2) - Math.abs(b.y);
+}
+
+Text.prototype.applyAttr = function(a){
+    this.element.attr(a);
+}
+
+Text.prototype.getAttr = function(a){
+    return this.element.attr(a);
+}
+
+Text.prototype.getWidth = function(){
+    
+    if(this.element == null)
+        return 0;
+    
+    var b = this.element.getBBox();
+
+    return  Math.abs(b.x2) - Math.abs(b.x);
+}
+
+Text.prototype.getHeight = function(){
+    
+    if(this.element == null)
+        return 0;
+    
+    var b = this.element.getBBox();
+
+    return  Math.abs(b.y2) - Math.abs(b.y);
+}
+
+Text.prototype.setX = function(x){
+    this.x = x;
+    
+    this.element.attr({"x":x});
+}
+
+Text.prototype.setY = function(y){
+    this.y = y;
+    
+    this.element.attr({"y":y});
+}
+
+Text.prototype.setLocation = function(x,y){
+    this.y = y;
+    this.x = x;
+    
+    this.element.attr({"y":y,"x":x});
+}
+
+Text.prototype.setDrawColor = function(color){
+    this.drawColor = color;
+    
+    this.element.attr({"fill":color});
+}
+
+Text.prototype.setFontFamily = function(fontFamily){
+    this.fontFamily = fontFamily;
+    
+    var font = this.fontSize + "px "+this.fontFamily;
+    
+    this.element.attr({"font":font});
+}
+
+Text.prototype.setFontSize = function(fontSize){
+    this.fontSize = fontSize;
+    
+    var font = this.fontSize + "px "+this.fontFamily;
+    
+    this.element.attr({"font":font});
+}
+
+Text.prototype.setText = function(text){
+    this.text = text;
+    
+    this.element.attr({"text":text});
+}
+
+Text.prototype.hide = function(){
+    this.element.hide();
+}
+
+Text.prototype.show = function(){
+    this.element.show();
 }
